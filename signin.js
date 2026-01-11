76669c3eeefe9be884a41db812e8d573c083db94
Vibin <vibinvigneshs@gmail.com>
2026-01-11 18:39:55 +0530

    connecting to flask server signin

--- signin.js
+++ signin.js
@@ -1,7 +1,21 @@
-  function no() {
-    window.location.href = "signin.html";
-  }
+function login(){
+  console.log("online..")
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
 
-  function yes(){
-    window.location.href = "newaccount.php";
-  }
+  fetch("https://ayla-ropier-consuela.ngrok-free.dev",{
+    method : "POST",
+    headers: {
+            "Content-Type": "application/json"
+        },
+    body: JSON.stringify({
+            user: username,
+            pass: password
+        })
+      })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+    });
+
+}
